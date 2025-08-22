1062b240399be4ecb9e4c2bfb4cad7186b85798e
Fenyep <0Uiimi1D5/-yYkGt>
2025-08-22 15:08:24 +0100

    Changed the favicon used

--- src/template.tsx
+++ src/template.tsx
@@ -7,10 +7,10 @@ export default function Template({ Head, Body, Script }: TemplateProps) {
 				<meta charSet="UTF-8" />
 
 				{/* <!-- Standard Favicon --> */}
-				<link rel="icon" type="image/png" href="/favicon/favicon-16x16.png" sizes="16x16" />
+				{/* <link rel="icon" type="image/png" href="/favicon/favicon-16x16.png" sizes="16x16" />
 				<link rel="icon" type="image/png" href="/favicon/favicon-32x32.png" sizes="32x32" />
-				<link rel="icon" type="image/svg+xml" href="/favicon/favicon2.svg" />
-				{/* <link rel="icon" type="image/x-icon" href="/makiti_group.ico" /> */}
+				<link rel="icon" type="image/svg+xml" href="/favicon/favicon2.svg" /> */}
+				<link rel="icon" type="image/x-icon" href="/makiti_group.ico" />
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 			</Head>
 
